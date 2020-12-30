9c67da4e47818168048b24a216e4efd79a1f79ba
jeo19 <terrakkk@outlook.com>
2020-12-30 12:05:54 +0800

    add:Add the Context state step 1

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.instanceCounter = 1;
-    this.state = {
-      stateCounter: 1,
-    };
-  }
-  setStateCounter = () => {
-    this.setState({
-      stateCounter: this.state.stateCounter + 1,
-    });
-    console.log(this.state.stateCounter);
-  };
-  setInstanceCounter = () => {
-    this.instanceCounter = this.instanceCounter + 1;
-    console.log(this.instanceCounter);
-  };
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <div>
-          State Counter: {this.state.stateCounter}{' '}
-          <button onClick={this.setStateCounter}>Inc State</button>
-        </div>
-
-        <div>
-          Instance Counter: {this.instanceCounter}
-          <button onClick={this.setInstanceCounter}>Inc Instance</button>
-        </div>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <LevelOne />
+    </div>
+  );
+}
+let contextData = 'hello';
+const Context = React.createContext(contextData);
+function onChange() {
+  contextData = 'goodbye';
+  Context._currentValue = 'goodbye';
+  console.log(contextData);
+}
+function LevelOne() {
+  return (
+    <Context.Provider value={contextData}>
+      <LevelTwo />
+    </Context.Provider>
+  );
+}
+function LevelTwo() {
+  return <LevelThree />;
+}
+function LevelThree() {
+  const context = useContext(Context);
+  return (
+    <div>
+      {' '}
+      <button onClick={onChange}>Change context</button>
+      {context}
+    </div>
+  );
 }
